bb35488fe982b1ff414108826020d6d07230f7dd
RnshJohn <a0985334801@gmail.com>
2021-06-01 11:57:23 +0800

    add swimming test

---
+++ app/src/main/java/main/swimmingPool.java
@@ -0,0 +1,62 @@
+package main;
+
+public class swimmingPool {
+	public int swimming(boolean weekend, int age, boolean  group,boolean primeM, int in_hour)throws Exception
+	{
+		int price;
+
+
+
+		if((in_hour >=5 && in_hour <= 22) && (age >=3 && age <= 75))
+		{
+			if(primeM == true)
+			{
+				if(weekend == true)
+				{
+					price = 125;
+
+				}
+				else
+				{
+					price = 100;
+
+				}
+			}else {
+				if(weekend == true)
+				{
+					price = 250;
+
+				}
+				else {
+					if(group == true)
+					{
+						price = 140;
+					}
+					else
+					{
+						if(in_hour < 7 || age >= 60 || age <= 12)
+						{
+							price = 160;
+						}
+						else {
+							price = 200;
+						}
+					}
+				}
+			}
+
+
+
+		}else {
+			throw new Exception("Can not come in");
+		}
+
+		return price;
+
+
+
+
+
+	}
+
+}
---
+++ app/src/test/java/main/swimmingPoolTest.java
@@ -0,0 +1,65 @@
+package main;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class swimmingPoolTest {
+
+
+  @Test
+  void test01() throws Exception {
+    swimmingPool s1 = new swimmingPool();
+    assertThrows(Exception.class,()->s1.swimming(false,2,true,false,6),"Can not come in");;
+  }
+
+  @Test
+  void test02() throws Exception {
+    swimmingPool s1 = new swimmingPool();
+    assertEquals(100, s1.swimming(false, 9, true, true,12));
+  }
+
+  @Test
+  void test03() throws Exception {
+    swimmingPool s1 = new swimmingPool();
+    assertThrows(Exception.class,()->s1.swimming(true, 30 ,false,false,23),"Can not come in");
+  }
+
+  @Test
+  void test04() throws Exception {
+    swimmingPool s1 = new swimmingPool();
+    assertEquals(125,s1.swimming(true, 65 ,false,true,12));
+  }
+
+  @Test
+  void test05() throws Exception {
+    swimmingPool s1 = new swimmingPool();
+    assertThrows(Exception.class,()->s1.swimming(false, 90 ,false,true,11),"Can not come in");
+  }
+
+
+  @Test
+  void test06() throws Exception {
+    swimmingPool s1 = new swimmingPool();
+    assertEquals(200,s1.swimming(false, 30 ,false,false,12));
+  }
+  @Test
+  void test07() throws Exception {
+    swimmingPool s1 = new swimmingPool();
+    assertEquals(140,s1.swimming(false, 30 ,true,false,12));
+  }
+
+  @Test
+  void test08() throws Exception {
+    swimmingPool s1 = new swimmingPool();
+    assertEquals(160,s1.swimming(false, 30 ,false,false,6));
+  }
+
+  @Test
+  void test09() throws Exception {
+    swimmingPool s1 = new swimmingPool();
+    assertEquals(160,s1.swimming(false, 65 ,false,false,12));
+  }
+}
